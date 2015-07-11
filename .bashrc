jzacsh/dotfiles

commit 24b7f38803358234624a3363aca2a3cb38a1f3b3

ensure EUID=0 interactive shell doesn't run EVERYTHINg. cleanup a LOT of cruft from bashrc

.bashrc
@@ -1,26 +1,12 @@
 # If not running interactively, don't do anything
 [[ ${-} = ${-/i/} ]] && return
 
-source ~/bin/share/zacsh_exports
-
-#colors:
-col_red='\[\e[1;31m\]'
-col_grn='\[\e[1;32m\]'
-col_blu='\[\e[1;34m\]'
-col_end='\[\e[m\]'
-#
-
-[[ -r ~/.bash_aliases ]] && source ~/.bash_aliases
-
-# set a fancy prompt (non-color, unless we know we "want" color)
-case "$TERM" in
-    xterm-color) color_prompt=yes;;
-esac
-
 # External config
 [[ -r ~/.dircolors && -x /bin/dircolors ]] && eval $(dircolors -b ~/.dircolors)
-[[ -r ~/.git-completion.bash ]] && source ~/.git-completion.bash
 [[ -r ~/.hgbashrc ]] && source ~/.hgbashrc
+for completion in ~/.config/bash_completion.d/*.sh; do
+  source "$completion"
+done
 
 # shell opts
 shopt -s cdspell
@@ -33,7 +19,7 @@ shopt -s dirspell
 # notify of completed background jobs immediately
 set -o notify
 set +o histexpand
-# set -o vi
+set -o vi
 
 # turn off control character echoing
 stty -ctlecho
@@ -41,7 +27,7 @@ stty -ctlecho
 # history options
 export HISTIGNORE="&:ls:[bf]g:exit:hg in:hg out:reset:clear:ca:cl:l:cd*"
 export HISTFILESIZE=2000
-export HISTCONTROL="ignoreboth"
+export HISTCONTROL='ignoreboth'
 export HISTSIZE=500
 
 #`who` else loged in:
@@ -50,10 +36,9 @@ if [[ $(who | grep -v $(whoami)) ]]; then
     who -H | grep -v $(whoami)
 fi
 
-#simple version of bash prompt:
-PS1='[\u@\h] ${?} $(vcprompt) \w\n\$ '
+PS1='[\u@\h] ${?} $(vcprompt) \w\n\$ ' # simple version of below
 
-#vcs and color-aware version of bash prompt:
+# vcs and color-aware version of bash prompt:
 bash_prompt() {
   case $TERM in
     xterm*|rxvt*)
@@ -62,11 +47,7 @@ bash_prompt() {
       local TITLEBAR="" ;;
   esac
 
-  #export PS1='$(uname -n)::$(pwd | tail -c 23)$ '
-  #
-  #colors:
   local col_end='\[\033[0m\]'
-
   local col_blk='\[\e[0;30m\]'
   local col_red='\[\e[1;31m\]'
   local col_grn='\[\e[1;32m\]'
@@ -75,27 +56,38 @@ bash_prompt() {
   local col_wht='\[\e[0;37m\]'
 
   local col_usr=$col_grn
-  [[ $UID -eq "0" ]] && col_usr=$col_red #root's color
+  if [[ $UID -eq "0" ]];then
+    col_usr=$col_red #root's color
+    alias vcprompt='echo -n' # don't execute `vcprompt`
+  fi
 
   RET_VALUE='$(if [[ $RET -ne 0 ]];then echo -n ":\[\033[1;31m\]$RET\[\033[0m\]";fi)'
   PS1="${TITLEBAR}${col_blu}${col_end}${col_usr}\u@${col_end}${col_blu}\h${col_end}${RET_VALUE}"' \[\033[0;32m\]$(vcprompt)\[\033[0m\]'" ${col_ylw}\w${col_end}\n${col_blu}${col_end}\t${col_ylw} \$${col_end} "
   PS4='+$BASH_SOURCE:$LINENO:$FUNCNAME: '
 }
-
-PROMPT_COMMAND='RET=$?'
 bash_prompt
-unset bash_prompt
 
-#dynamic config:
-source $HOME/.host/pick
+
+#
+# $UID > 0 BELOW THIS LINE
+#   everything above *should* be plain old shell options, nothing executable
+#
+[[ "$UID" -eq "0" ]] && return
+
+
+source ~/bin/share/zacsh_exports
+[[ -r ~/.bash_aliases ]] && source ~/.bash_aliases
+
+# for irssi's notify.pl
+[[ -n $DBUS_SESSION_BUS_ADDRESS ]] &&
+  echo "$DBUS_SESSION_BUS_ADDRESS" > ~/.dbus_address
+
+source $HOME/.host/pick  # Dynamic config
 
 addkeys #load ssh keys
 
-#make sure dropbox is running
-updropboxd
-
 #must be after PATH:, apparently this will break if non-interactive shell `return`'s above.
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" || true
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 #in my nested tmux shells, my inherited `env` is old
 { DBUS_SESSION_BUS_ADDRESS="$(< ~/.dbus_address)"; } 2>/dev/null
@@ -109,3 +101,6 @@ updropboxd
 #      2>/dev/null & &>/dev/null
 #  sleep 1
 #  kill $! &>/dev/null
+
+
+true # don't assume last return status

.bash_completion
@@ -1,22 +1,0 @@
-#!/bin/bash
-
-_git_commit () {
-  local cur prev ng_set
-
-  # do nothing if we're not in a repo
-  git rev-parse 2>/dev/null || return 0
-
-  COMPREPLY=()
-  cur=${COMP_WORDS[COMP_CWORD]}
-  prev=${COMP_WORDS[COMP_CWORD-1]}
-
-  # complete uncommitted files
-  if [[ ${COMP_WORDS[1]} = "commit" ]]; then
-    COMPREPLY=( $(compgen -W "\
-                $( while read file; do
-                     echo ${file:2}
-                   done < <(git status -s) )" -- $cur ) )
-  fi
-}
-
-complete -o filenames -F _pacman spp pp

.bash_profile
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-#make sure irssi's notify.pl knows dbus address
-if [[ -n $DBUS_SESSION_BUS_ADDRESS ]];then
-    echo "$DBUS_SESSION_BUS_ADDRESS" > ~/.dbus_address
-fi
-
 #if interactive, source .bashrc
 [[ -n $PS1 && -f ~/.bashrc ]] && source ~/.bashrc
 

.config/bash_completion.d/common-completion.sh
@@ -1,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Bash Completion functions
+#
+# From: man bash | less +/^\ *Programmable\ Completion
+#     If a shell function is being invoked, the COMP_WORDS and COMP_CWORD
+#     variables are also set.  When the function or command is invoked, the
+#     first argument ($1) is the name of the command whose arguments are being
+#     completed, the second argument  ($2)  is  the  word being completed, and
+#     the third argument ($3) is the word preceding the word being completed on
+#     the current command line.  No filtering of the generated completions
+#     against the word being completed is performed; the function or command
+#     has complete freedom in generating the matches.
+#
+# More on `complete` builtin:
+#   man bash | less +/^\ *SHELL BUILTIN COMMANDS
+#   help complete # kind of empty
+
+
+_git_commit () {
+  local cur prev ng_set
+
+  # do nothing if we're not in a repo
+  git rev-parse 2>/dev/null || return 0
+
+  COMPREPLY=()
+  cur=${COMP_WORDS[COMP_CWORD]}
+  prev=${COMP_WORDS[COMP_CWORD-1]}
+
+  # complete uncommitted files
+  if [[ ${COMP_WORDS[1]} = "commit" ]]; then
+    COMPREPLY=( $(compgen -W "\
+                $( while read file; do
+                     echo ${file:2}
+                   done < <(git status -s) )" -- $cur ) )
+  fi
+}
+
+# TODO(zacsh) Figure out hwo to get this working
+# complete -E -o filenames -F _git_commit

.host/distro.ubuntu
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-set -o vi
 export BROWSER=chromium-browser
-export DEBFULLNAME="Jonathan Zacsh"
-export DEBEMAIL="jzacsh@gmail.com"
-export TERM=xterm #ridiculousness for ubuntu's tmux
+export DEBFULLNAME='Jonathan Zacsh'
+export DEBEMAIL='jzacsh@gmail.com'
 export JAVA_HOME='/usr/lib/jvm/java-6-openjdk-amd64/'
 export CLOSURE_COMPILER="$HOME/usr/lib/js/closure-compiler"
 
-alias rw='for serv in varnish apache2 mysql; do sudo service "$serv" restart; done'
-
-alias pman="pydoc3.1"
-alias pdb="pdb3.1"
+alias pman='pydoc3.1'
+alias pdb='pdb3.1'
 
 #specific configs:
+
 ln -sf ~/.host/src/distro.ubuntu/.pbuilderrc ~/
 
 # vim: et:ts=2:sw=2

commit 3e1ae2afadf2d95149517111e7c2233be17ca04c

fixing PROMPT bug on "$?",caused by 24b7f388

.bashrc
@@ -65,7 +65,9 @@ bash_prompt() {
   PS1="${TITLEBAR}${col_blu}${col_end}${col_usr}\u@${col_end}${col_blu}\h${col_end}${RET_VALUE}"' \[\033[0;32m\]$(vcprompt)\[\033[0m\]'" ${col_ylw}\w${col_end}\n${col_blu}${col_end}\t${col_ylw} \$${col_end} "
   PS4='+$BASH_SOURCE:$LINENO:$FUNCNAME: '
 }
+PROMPT_COMMAND='RET=$?' # see: man bash | less +/PROMPT_COMMAND
 bash_prompt
+unset bash_prompt
 
 
 #