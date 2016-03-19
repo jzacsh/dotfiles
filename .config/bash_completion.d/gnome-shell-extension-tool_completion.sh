100de06126200c776c33769080c226f8a6a95f21
Jonathan Zacsh <jzacsh@gmail.com>
2016-03-19 19:08:50 -0400

    dynamic auto-completion of gnome-shell-extension-tool with -e/-d based no what is actualy enabled and available

--- .config/bash_completion.d/gnome-shell-extension-tool_completion.sh
+++ .config/bash_completion.d/gnome-shell-extension-tool_completion.sh
@@ -3,11 +3,7 @@
 # Autocompletion for gnome-shell-extension-tool
 #
 
-__get_avilable_extensions() {
-  for dir in {~/.local,/usr/}/share/gnome-shell/extensions/*; do
-    basename "$dir"
-  done
-}
+__scrapeJsonArray() { \sed -e "s|^\['||" -e "s|'\]$||" -e "s|', '|\n|g"; }
 
 # Per: man bash | less +/'^\s*Programmable\ Completion'
 #   When the function or command is invoked, the first argument ($1) is the name
@@ -17,8 +13,30 @@ __get_avilable_extensions() {
 _gset_completion() {
   { [ "$3" = -e ] || [ "$3" = -d ]; } || return 0
 
-  local exts; exts="$(__get_avilable_extensions)"
-  COMPREPLY=( $(compgen -W "$exts" -- "${COMP_WORDS[$COMP_CWORD]}") )
+  # extensions already enabled in gnome-shell
+  local nabled
+  nabled="$(dconf read /org/gnome/shell/enabled-extensions | __scrapeJsonArray)"
+
+  # extensions available on this machine
+  local avail; avail="$(
+    for dir in {~/.local,/usr/}/share/gnome-shell/extensions/*; do
+      basename "$dir"
+    done
+  )"
+
+  local results
+  if [ "$3" = -e ];then
+    results="$(
+      \diff \
+        <(printf '%s\n' "${avail[@]}" | sort) \
+        <(printf '%s\n' "${nabled[@]}" | sort) \
+       | \grep -- '^<' | \sed -e 's|^< ||'
+    )"
+  elif [ "$3" = -d ];then
+    results="$nabled"
+  fi
+
+  COMPREPLY=( $(compgen -W "$results" -- "${COMP_WORDS[$COMP_CWORD]}") )
 }
 
 complete -F _gset_completion gnome-shell-extension-tool
