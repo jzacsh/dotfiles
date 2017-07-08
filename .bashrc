jzacsh/dotfiles

commit 681445ef455a8895ace2a9dcd5d2514c3cfc8b4b

duplicate: already had a simplified version a few lines down

.bashrc
@@ -26,8 +26,6 @@ export HISTFILESIZE=20000
 export HISTCONTROL='ignoreboth'
 export HISTSIZE=5000
 
-PS1='sh#$SHLVL [\u@\h] ${?} \w\n\$ ' # vanilla version of my prompt, with no executables
-
 if [[ -r ~/.dircolors ]] && type dircolors >/dev/null 2>&1;then
   eval $(dircolors --bourne-shell ~/.dircolors)
 fi

commit e0943b2a22fd31e560eb15b0e6f5043edbfd3ac4

bugfix: give root decent prompt
caused by 681445ef455a8895a; not technically a *dup*; just not DRY. hopefully
clearer now. and a better prompt

.bashrc
@@ -30,6 +30,8 @@ if [[ -r ~/.dircolors ]] && type dircolors >/dev/null 2>&1;then
   eval $(dircolors --bourne-shell ~/.dircolors)
 fi
 
+PROMPT_COMMAND='RET=$?' # see: man bash | less +/PROMPT_COMMAND
+PS1='\s^$RET  @\t \w\n\u@\h   $SHLVL:\$ ' # vcprompt-less version of below
 ############################################################################
 # $UID > 0 BELOW THIS LINE
 #   everything above *should* be plain old shell options, nothing executable
@@ -37,7 +39,6 @@ fi
 ############################################################################
 
 
-PROMPT_COMMAND='RET=$?' # see: man bash | less +/PROMPT_COMMAND
 PS1='\s^$RET  @\t $(vcprompt) \w\n\u@\h   $SHLVL:\$ ' # simple version of below
 # vcs and color-aware version of bash prompt:
 bash_prompt() {